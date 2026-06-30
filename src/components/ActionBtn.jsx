4dda4581f7ab58f3744d26495ac7edbe75b68e33
Hritik Srivastava <srivastavaarpit1105@gmail.com>
2026-06-30 13:49:27 +0530

    fix: prevent error when no file is selected

--- src/components/ActionBtn.jsx
+++ src/components/ActionBtn.jsx
@@ -7,20 +7,24 @@ const ActionBtn = () => {
   const {setPreviewimg, setFile} = useContext(ImageSrc)
 
   const imagePreview = (dets)=>{
-    const selectedFile = dets.target.files[0]
+    if(dets.target.files[0]){
 
-    setFile(selectedFile)
+      const selectedFile = dets.target.files[0]
+  
+      setFile(selectedFile)
+  
+      const url = URL.createObjectURL(selectedFile)
+  
+      setPreviewimg(url)
 
-    const url = URL.createObjectURL(selectedFile)
-
-    setPreviewimg(url)
+    }
   }
 
   return (
       <div className="actionBtn lg:mt-[2vw] mt-[8vw]">
 
         <label htmlFor="uploadInput" className="bg-white lg:h-[4vw] lg:w-[30vw] h-[12vw] w-[75vw] rounded-xl cursor-pointer flex justify-center items-center shadow-black shadow-md">
-            <h3 className="text-black border-dashed border-2 border-gray-400 lg:px-20 lg:py-2 px-3 py-0.5 rounded-md">Click or Drag to Upload your Image</h3>
+            <h3 className="text-black border-dashed border-2 border-gray-400 lg:px-20 lg:py-2 px-3 py-0.5 rounded-md">Click to Upload your Image</h3>
             <input type="file" id="uploadInput" accept="image/*" hidden onChange={imagePreview}/>
         </label>
 
